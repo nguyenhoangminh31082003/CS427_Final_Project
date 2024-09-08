d5be61ce490bcf5fd6fef2a91fb445ba2e370d99
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-09-08 22:41:08 +0700

    Add rigidbody to player and detect collisions between player and computer

--- final-project/Assets/Machines/computer.cs
+++ final-project/Assets/Machines/computer.cs
@@ -2,17 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class computer : MonoBehaviour
+public class Computer : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField]
+    private GameObject screenExplosionParticleSystem;
+    [SerializeField]
+    private GameObject screenOff;
+    [SerializeField]
+    private GameObject screenOn;
+    [SerializeField]
+    private GameObject shards;
+    private bool broken;
+
+    private void Awake()
+    {
+        this.broken = false;
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private bool Explode()
+    {
+        if (this.broken)
+            return false;
+
+        this.broken = true;
+        this.screenOff.SetActive(false);
+        this.screenOn.SetActive(false);
+        this.shards.SetActive(true);
+        Rigidbody[] shardRBs = this.GetComponentsInChildren<Rigidbody>();
+        this.screenExplosionParticleSystem.SetActive(true);
+        foreach (Rigidbody shardRB in shardRBs)
+        {
+            float randomForce = Random.Range(1, 5);
+            float randomRotationX = Random.Range(-20, 20);
+            float randomRotationY = Random.Range(-20, 20);
+            float randomRotationZ = Random.Range(-20, 20);
+            shardRB.transform.Rotate(randomRotationX, randomRotationY, randomRotationZ);
+            shardRB.AddRelativeForce(Vector3.forward * randomForce, ForceMode.Impulse);
+        }
+
+        return true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("GameObject Player has entered the sphere collider of GameObject Computer.");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("GameObject Player has exited the sphere collider of GameObject Computer.");
+        }
     }
 }
