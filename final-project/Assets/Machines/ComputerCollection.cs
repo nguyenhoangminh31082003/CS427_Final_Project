69d6e96ab8fabeeace187ce77800c5b395c40e62
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-09-09 16:49:36 +0700

    Add cars to central region

--- final-project/Assets/Machines/ComputerCollection.cs
+++ final-project/Assets/Machines/ComputerCollection.cs
@@ -1,10 +1,11 @@
+using System;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class ComputerCollection : MonoBehaviour
 {
-
+    [SerializeField] private int numberOfChosenComputers;
     private GameObject[] computers;
 
     void Start()
@@ -12,8 +13,12 @@ public class ComputerCollection : MonoBehaviour
         if (this.computers == null)
             this.computers = GameObject.FindGameObjectsWithTag("Computer");
 
-        foreach (GameObject computer in this.computers)
-            Debug.Log(computer.name);
+        this.numberOfChosenComputers = Math.Min(Math.Max(0, numberOfChosenComputers), this.computers.Length);
+    }
+
+    public int GetNumberOfChosenComputers()
+    {
+        return this.numberOfChosenComputers;
     }
 
     void Update()
