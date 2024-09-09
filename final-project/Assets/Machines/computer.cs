3fd483e309521916fb7240161d71fb87a8f972c3
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-09-09 15:36:41 +0700

    Add method to retrieve status of computers

--- final-project/Assets/Machines/computer.cs
+++ final-project/Assets/Machines/computer.cs
@@ -91,4 +91,14 @@ public class Computer : MonoBehaviour
             }
         }
     }
+
+    public bool IsBroken()
+    {
+        return this.broken;
+    }
+
+    public bool IsBreakable()
+    {
+        return this.breakable; 
+    }
 }
