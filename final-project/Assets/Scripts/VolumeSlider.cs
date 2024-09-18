6687a25c3a1e7c2635202177778136ada828eea6
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-09-19 00:33:14 +0700

    Allow players to set volume

--- final-project/Assets/Scripts/VolumeSlider.cs
+++ final-project/Assets/Scripts/VolumeSlider.cs
@@ -11,8 +11,26 @@ public class VolumeSlider : MonoBehaviour
     [SerializeField] private TextMeshProUGUI sliderText;
     void Start()
     {
-        slider.onValueChanged.AddListener((newVolume) => {
+        float savedVolume = PlayerPrefs.GetFloat("VolumeLevel", 1f);
+
+        slider.value = savedVolume * 100;
+
+        AudioListener.volume = savedVolume;
+
+        sliderText.text = (savedVolume * 100) + "%";
+
+        slider.onValueChanged.AddListener((newVolume) =>
+        {
+            // Convert slider value (0-100) to a percentage (0.0 - 1.0)
+            float volume = newVolume / 100;
+            AudioListener.volume = volume;
+
+            // Update text
             sliderText.text = newVolume + "%";
+
+            // Save the volume level so it's retained between scenes or sessions
+            PlayerPrefs.SetFloat("VolumeLevel", volume);
+            PlayerPrefs.Save();
         });
     }
 
