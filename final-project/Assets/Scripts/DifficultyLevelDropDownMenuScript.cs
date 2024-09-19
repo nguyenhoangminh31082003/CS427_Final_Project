7364ee6653cc27fdb618a4f0e5831b9190472e02
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-09-19 12:16:00 +0700

    Add volume slider to in-game menu

--- final-project/Assets/Scripts/DifficultyLevelDropDownMenuScript.cs
+++ final-project/Assets/Scripts/DifficultyLevelDropDownMenuScript.cs
@@ -1,18 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class DifficultyLevelDropDownMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Dropdown difficultyDropdown;  // Reference to the Dropdown
+
+    // Key used to store and retrieve the selected difficulty level
+    private const string DifficultyPrefKey = "DifficultyLevel";
+    private const string InitialMinutesPrefKey = "InitialMinutes";
+
     void Start()
     {
-        
+        // Load the saved difficulty level when the game starts
+        this.LoadDifficulty();
+
+        // Add a listener to detect dropdown value changes
+        difficultyDropdown.onValueChanged.AddListener(delegate {
+            SaveDifficulty(difficultyDropdown.value);
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    // Save the selected difficulty level to PlayerPrefs
+    void SaveDifficulty(int difficultyValue)
     {
-        
+        PlayerPrefs.SetInt(DifficultyPrefKey, difficultyValue);
+     
+        if (difficultyValue == 0)
+            PlayerPrefs.SetInt(InitialMinutesPrefKey, 60);
+        else if (difficultyValue == 1)
+            PlayerPrefs.SetInt(InitialMinutesPrefKey, 30);
+        else if (difficultyValue == 2)
+            PlayerPrefs.SetInt(InitialMinutesPrefKey, 15);
+
+        PlayerPrefs.Save();
+    }
+
+    // Load the saved difficulty level and set the dropdown to the correct option
+    void LoadDifficulty()
+    {
+        if (PlayerPrefs.HasKey(DifficultyPrefKey))
+        {
+            int savedDifficulty = PlayerPrefs.GetInt(DifficultyPrefKey);
+            difficultyDropdown.value = savedDifficulty;
+        }
+        else
+        {
+            // If there's no saved preference, you can set it to a default value (e.g., 0)
+            difficultyDropdown.value = 1;  // Assuming 0 is the default
+            this.SaveDifficulty(difficultyDropdown.value);
+        }
     }
 }
